9d32ff99cf1d4acd4ca34de56be5f12b637646f7
apol <adripol1994@gmail.com>
2017-01-27 13:04:54 +0100

    asd

---
+++ app/src/main/java/es/iesnervion/qa/DAL/Connection.java
@@ -0,0 +1,20 @@
+package es.iesnervion.qa.DAL;
+
+import es.iesnervion.qa.Model.Categoria;
+
+/**
+ * Created by apol on 27/01/17.
+ */
+
+public class Connection {
+
+    /**
+     * Devuelve todas las categorias disponibles en la API
+     * @return Categoria array
+     */
+    public static Categoria[] getCategorias() {
+
+
+        return null;
+    }
+}
